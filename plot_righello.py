e29c4bb71cf1e977c3ee07faa39192060c5d65d9
Valeria Scarabelli <v.scarabelli@icloud.com>
2026-04-23 15:16:24 +0200

    farneticazioni

--- plot_righello.py
+++ plot_righello.py
@@ -8,7 +8,7 @@ x_data = list (range(1, len(y_data) + 1))
 fig, ax = plt.subplots()
 
 ax.set_title("LDR data")
-ax.set_xlabel("Time (ms)")
+ax.set_xlabel("Time ($10^{-1}$ s)")
 ax.set_ylabel("LDR value")
 
 ax.plot(x_data, y_data, label="LDR", marker = '', color='blue', linewidth=1)
